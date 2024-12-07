BUGFIX on scale parameter save/read; implement precision from rot/scale; stdev tuning; 1stdev 180FPS on 3500 points

baseline/fm_render.py
@@ -76,14 +76,14 @@ def render_func_rays(camera_starts_rays, means, prec_full, weights_log, beta_2, 
             return res, d2
 
         # runs parallel for each ray across each gaussian
-        res, d2 = jax.lax.map(perf_ray, camera_starts_rays, batch_size=32)
+        res, d2 = jax.lax.map(perf_ray, camera_starts_rays, batch_size=16)
 
         return res, d2
 
     # runs parallel for gaussian
     # zs, stds, projp = jax.vmap(perf_idx)(prec, weights_log, means)
     args = {0: prec, 1: weights_log, 2: means}
-    zs, stds = jax.lax.map(perf_idx, args, batch_size=32)
+    zs, stds = jax.lax.map(perf_idx, args, batch_size=16)
 
     # alpha is based on distance from all gaussians
     est_alpha = 1 - jnp.exp(-jnp.exp(stds).sum(0))

baseline/ycb_fmb_rotscale.py
@@ -153,6 +153,7 @@ for NUM_MIXTURE in [150, 500, 1500, 2500, 3500]:
     )  # identity rotation
     _rot = jax.lax.map(quat_to_rot, gaussian_quat, batch_size=256)  # identity rotation
     scale = np.array([1 / jnp.sqrt(5e3) * np.eye(3) for _ in range(NUM_MIXTURE)])
+    scale_diag = np.array([[s[0, 0], s[1, 1], s[2, 2]] for s in scale])
 
     rs = jnp.einsum(
         "aij,ajk->aik", _rot, scale
@@ -162,6 +163,8 @@ for NUM_MIXTURE in [150, 500, 1500, 2500, 3500]:
     prec_sqrt = np.linalg.cholesky(
         prec
     )  # A = L L^T for real A, L lower triangular with positive diagonal
+
+    del scale
 
     # camera
     fovX = jnp.arctan(intrinsics.width / 2 / intrinsics.fx) * 2.0
@@ -240,7 +243,7 @@ for NUM_MIXTURE in [150, 500, 1500, 2500, 3500]:
     )
     np.asarray(mean, dtype=np.float32).ravel().tofile(f"{directory}/means.bin")
     np.asarray(gaussian_quat, dtype=np.float32).ravel().tofile(f"{directory}/quats.bin")
-    np.asarray(scale, dtype=np.float32).ravel().tofile(f"{directory}/scales.bin")
+    np.asarray(scale_diag, dtype=np.float32).ravel().tofile(f"{directory}/scales.bin")
     np.asarray(cov, dtype=np.float32).ravel().tofile(f"{directory}/covs.bin")
     np.asarray(prec_sqrt, dtype=np.float32).ravel().tofile(f"{directory}/precs.bin")
     np.asarray(weights_log, dtype=np.float32).ravel().tofile(f"{directory}/weights.bin")